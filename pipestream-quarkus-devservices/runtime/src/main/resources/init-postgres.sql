f759d199216f8de8d04088ac38ddfc8bbdd3be26
Kristian Rickert <krickert@gmail.com>
2025-12-21 17:37:19 -0500

    feat!: remove MySQL, migrate Apicurio to PostgreSQL
    - Remove MySQL service from compose-devservices.yml
    - Update Apicurio Registry to use PostgreSQL storage
    - Add apicurio_registry database to init-postgres.sql
    - Delete init-mysql.sql (no longer needed)
    
    All services now use PostgreSQL exclusively.

--- pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
+++ pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
@@ -3,12 +3,14 @@
 
 -- Create databases
 CREATE DATABASE infisical;
+CREATE DATABASE apicurio_registry;
 CREATE DATABASE pipeline_repository_dev;
 CREATE DATABASE pipeline_repository_test;
 CREATE DATABASE pipeline_connector_dev;
 
 -- Grant privileges to pipeline user
 GRANT ALL PRIVILEGES ON DATABASE infisical TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE apicurio_registry TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE pipeline_repository_dev TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE pipeline_repository_test TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE pipeline_connector_dev TO pipeline;

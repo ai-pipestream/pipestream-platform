24df68f0d615d021ca1270a3ec12594e9dd85e7d
Kristian Rickert <krickert@gmail.com>
2026-01-19 13:02:56 -0500

    added more databases and corrected permissions for quarkus dev mode

--- pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
+++ pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
@@ -9,12 +9,16 @@ CREATE DATABASE pipeline_connector_dev;
 CREATE DATABASE pipeline_account_dev;
 CREATE DATABASE pipeline_engine_dev;
 CREATE DATABASE pipeline_connector_intake_dev;
+CREATE DATABASE pipeline_s3_connector_dev;
 
 
 -- Grant privileges to pipeline user
 GRANT ALL PRIVILEGES ON DATABASE infisical TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE apicurio_registry TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE pipeline_repository_dev TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_repository_test TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE pipeline_connector_dev TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE pipeline_account_dev TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE pipeline_engine_dev TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE pipeline_connector_intake_dev TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE pipeline_s3_connector_dev TO pipeline;
 

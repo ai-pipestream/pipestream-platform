a99c7b887eec46bd27e4d9450d0e62f944cdb656
Kristian Rickert <krickert@gmail.com>
2026-01-19 08:47:58 -0500

    adding databases that were not there yet - they were using the pipeline db, now each service will have a dedicated db.

--- pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
+++ pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
@@ -5,8 +5,11 @@
 CREATE DATABASE infisical;
 CREATE DATABASE apicurio_registry;
 CREATE DATABASE pipeline_repository_dev;
-CREATE DATABASE pipeline_repository_test;
 CREATE DATABASE pipeline_connector_dev;
+CREATE DATABASE pipeline_account_dev;
+CREATE DATABASE pipeline_engine_dev;
+CREATE DATABASE pipeline_connector_intake_dev;
+
 
 -- Grant privileges to pipeline user
 GRANT ALL PRIVILEGES ON DATABASE infisical TO pipeline;

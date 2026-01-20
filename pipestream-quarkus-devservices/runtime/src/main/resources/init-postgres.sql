9d839adb54dd1801826a69d4bb450458f00737c8
Kristian Rickert <krickert@gmail.com>
2026-01-20 04:29:39 +0000

    Use simple database naming convention in init-postgres.sql

--- pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
+++ pipestream-quarkus-devservices/runtime/src/main/resources/init-postgres.sql
@@ -1,28 +1,24 @@
 -- PostgreSQL initialization script for compose-devservices
--- Creates additional databases for services
+-- Creates additional databases for services (simple naming convention)
 
 -- Create databases
 CREATE DATABASE infisical;
 CREATE DATABASE apicurio_registry;
-CREATE DATABASE pipeline_repository_dev;
-CREATE DATABASE pipeline_connector_dev;
-CREATE DATABASE pipeline_account_dev;
-CREATE DATABASE pipeline_engine_dev;
-CREATE DATABASE pipeline_connector_intake_dev;
-CREATE DATABASE pipeline_s3_connector_dev;
 CREATE DATABASE platform_registration;
 CREATE DATABASE connector_intake;
-
+CREATE DATABASE connector_admin;
+CREATE DATABASE account;
+CREATE DATABASE repository;
+CREATE DATABASE engine;
+CREATE DATABASE s3_connector;
 
 -- Grant privileges to pipeline user
 GRANT ALL PRIVILEGES ON DATABASE infisical TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE apicurio_registry TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_repository_dev TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_connector_dev TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_account_dev TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_engine_dev TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_connector_intake_dev TO pipeline;
-GRANT ALL PRIVILEGES ON DATABASE pipeline_s3_connector_dev TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE platform_registration TO pipeline;
 GRANT ALL PRIVILEGES ON DATABASE connector_intake TO pipeline;
-
+GRANT ALL PRIVILEGES ON DATABASE connector_admin TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE account TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE repository TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE engine TO pipeline;
+GRANT ALL PRIVILEGES ON DATABASE s3_connector TO pipeline;
